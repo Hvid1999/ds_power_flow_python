fixed a bug in Jacobian calculation. Example from lecture 7 and example 10.6 from Vittal are now working.

pf_test.py
@@ -1,14 +1,17 @@
 import power_flow_newton_raphson as pf
 import numpy as np
 
-##Working example: Vittal example 10.6
-#Realized that it was probably wiser to program using the vectors 
-#v, delta, d_p, d_q and stack them when necessary for calculations
 
+# To be written: Tests comparing custom solver results to PandaPower results using their test cases
+# Will probably need to implement a function to check reactive power limits for PV busses
+###################################################################################################
+
+""" 
 ##initialization
 slack_bus_idx = 0 #slack bus placement - bus 1 is bus 0 in the code
 pv_idx = np.array([1])
 iteration_limit = 15
+tolerance = 0.001 #tolerance level for error
 
 #bus admittance matrix and system size
 ybus = np.array([[complex(0,-19.98),complex(0,10),complex(0,10)],[complex(0,10),complex(0,-19.98),complex(0,10)],[complex(0,10),complex(0,10),complex(0,-19.98)]])
@@ -43,26 +46,35 @@ jacobian_calc = pf.simplify_jacobian(n_buses, pv_idx, jacobian)
 print("J: \n", np.round(jacobian_calc, 2))
 
 
-""" #calculate next iteration of voltage magnitude and phase angle
-(delta, vmag) = pf.next_iteration(jacobian_calc, vmag, delta, del_p, del_q)
-print("delta_next:\n", delta * 180/np.pi)
-print("vmag_next:\n", vmag) """
-
-
 for i in range(1, iteration_limit + 1):
     (delta, vmag) = pf.next_iteration(jacobian_calc, vmag, delta, del_p, del_q)
     #Calculating initial power vectors
     delta_full[1:] = delta
     vmag_full[2] = vmag #PLACEHOLDER - NEEDS LOGIC FOR OTHER SIZES!!!
     (p, q, p_full, q_full) = pf.calculate_power_vecs(n_buses, vmag_full, delta_full, b, g, pv_idx)
+
+    pf.calculate_jacobian(n_buses, jacobian, vmag_full, delta_full, g, b, p_full, q_full)
+    #simplify Jacobian according to PV-busses
+    jacobian_calc = pf.simplify_jacobian(n_buses, pv_idx, jacobian)
+
     (del_p, del_q) = pf.update_mismatch_vector(p, q, pset, qset)
     y = np.row_stack((del_p, del_q))
-    if pf.check_convergence(y, 0.001):
+
+    print("\nIteration %d:\n" % i)
+    print("delta:\n",delta * 180/np.pi)
+    print("vmag:\n",vmag)
+    print("mismatch vector:\n", y)
+    print("Jacobian:\n", jacobian_calc)
+
+    if pf.check_convergence(y, tolerance):
         print("Power flow converged at %d iterations.\n" % i)
         print("delta:\n",delta * 180/np.pi)
         print("vmag:\n",vmag)
+        print("Real power flows excluding slack:\n", p_full[1:])
+        print("Reactive power flows excluding slack:\n", q_full[1:])
         print("mismatch vector:\n", y)
         break
     
     elif i == iteration_limit:
         print("Power flow did not converge after %d iterations.\n" % i )
+ """

pf_test_vittal_example_10_6.py
@@ -1,26 +1,29 @@
 import power_flow_newton_raphson as pf
 import numpy as np
 
-##Working example: Lecture 7 (2-bus system)
+##Working example: Vittal example 10.6
 #Realized that it was probably wiser to program using the vectors 
 #v, delta, d_p, d_q and stack them when necessary for calculations
 
 ##initialization
 slack_bus_idx = 0 #slack bus placement - bus 1 is bus 0 in the code
-pv_idx = np.array([]) #the system has no PV-busses, so an empty array is initialized
-
+pv_idx = np.array([1])
 iteration_limit = 15
-tolerance = 0.001 #tolerance level for mismatches
+tolerance = 0.001 #tolerance level for error
 
-ybus = np.array([[complex(0,-10),complex(0,10)],[complex(0,10),complex(0,-10)]])
+#bus admittance matrix and system size
+ybus = np.array([[complex(0,-19.98),complex(0,10),complex(0,10)],[complex(0,10),complex(0,-19.98),complex(0,10)],[complex(0,10),complex(0,10),complex(0,-19.98)]])
+
 #power and voltage setpoints
-pset = np.array([-2.0]) #P setpoints as injections for every bus except slack bus in ascending bus order
-qset = np.array([-1.0]) #Q setpoints as injections for every bus except slack bus and PV buses in ascending bus order
+pset = np.array([0.6661, -2.8653]) #P setpoints as injections for every bus except slack bus in ascending bus order
+qset = np.array([-1.2244]) #Q setpoints as injections for every bus except slack bus and PV buses in ascending bus order
+vset = np.array([1.05]) #voltage setpoints for every PV bus in ascending bus order
+vset.shape = (np.size(vset),1)
 qset.shape = (np.size(qset),1)
 pset.shape = (np.size(pset),1)
 
 #setup system variables
-(jacobian, n_buses, vmag, delta, vmag_full, delta_full, g, b, p, q, del_p, del_q) = pf.initialize_system(ybus, pset, qset, pv_idx, 0)
+(jacobian, n_buses, vmag, delta, vmag_full, delta_full, g, b, p, q, del_p, del_q) = pf.initialize_system(ybus, pset, qset, pv_idx, vset)
 
 #noting indices of PQ-busses based on slack bus and PV-bus indices  
 pq_idx = np.arange(n_buses, dtype=int)
@@ -44,9 +47,10 @@ print("J: \n", np.round(jacobian_calc, 2))
 for i in range(1, iteration_limit + 1):
     (delta, vmag) = pf.next_iteration(jacobian_calc, vmag, delta, del_p, del_q)
     #Calculating initial power vectors
+    
     delta_full[1:] = delta #updating voltage angles on all busses except slack
     vmag_full[pq_idx] = vmag #updating voltage magnitudes on PQ-busses
-
+    
     (p, q, p_full, q_full) = pf.calculate_power_vecs(n_buses, vmag_full, delta_full, b, g, pv_idx)
 
     pf.calculate_jacobian(n_buses, jacobian, vmag_full, delta_full, g, b, p_full, q_full)

power_flow_newton_raphson.py
@@ -1,54 +1,19 @@
 """
-Pseudocode:
+PSEUDOCODE
 
 
+----------------------------------------------------------------------------------
 FUNCTIONS:
-
-
-MAYBE THE INITIALIZING FUNCTION SHOULD BE SPLIT INTO SEVERAL FUNCTIONS AND CALL EACH OF THEM:
-    Function 1: Create Y_bus
-    Function 2: Setup initial values and bus types with power limits
-    Function 3: Initial iteration of power mismatch 
-              (should account for whether bus is load or generation (+/- on pset/qset))
-    Function 4: Setup Jacobian
-
-
-def initialize_system(, custom_initial = False):
-    Load system bus admittance matrix
-    Define system size - N buses
-    Load input data values for P, Q, V, delta
-    Load system limits for real and reactive power
-    Setup initial guesses - custom or standard (1.0 mag and 0.0 angle)
-    Setup initial power mismatch vector delta_y
-    Setup initial Jacobian matrix (dimensions)
-
-
-def calculate_Jacobian(n_buses, jacobian, x, adm_mat):
-    recalculate Jacobian matrix based on new iterative values
-
-    Slicing the jacobian in order to handle the submatrices
-
-    Diagonal elements
-
-    Off-diagonal elements
-
-
-def next_iteration():
-    Invert the Jacobian - for now, faster methods are not necessary
-    Solve for x(i+1)
-    Return new x-vector
-
-def check_convergence():
-    check for convergence based on power mismatches and tolerance level
-    return boolean
 
 def pv_bus_check():
     calculate reactive power Q at PV-busses and check if within predefined limits
-    if not, set bus to PQ at Q limit and compute bus voltage
+    if not, set bus to PQ at Q limit and redo the power flow
 
 
 
+----------------------------------------------------------------------------------
 CLASSES:
+
 
 class Bus:
     Contains the following information:
@@ -57,15 +22,6 @@ class Bus:
         Reactive power injection
         Voltage magnitude
         Voltage phase angle
-
-(...
-class Generator:
-    Contains the following information:
-        Bus index placement
-        Power setpoints
-        Reactive power limits
-        (slack participation factor)
-...)
 
 ...
 
@@ -138,9 +94,18 @@ def calculate_jacobian(n_buses, jacobian, vmag, delta, g, b, p, q):
 
             else: #off-diagonal elements
                 j1[k-1][n-1] = vmag[k] * vmag[n] * (g[k][n]*(np.sin(delta[k] - delta[n])) - b[k][n]*np.cos(delta[k] - delta[n]))
-                j2[k-1][n-1] = -vmag[k] * vmag[n] * (g[k][n]*(np.cos(delta[k] - delta[n])) + b[k][n]*np.sin(delta[k] - delta[n]))
-                j3[k-1][n-1] = vmag[k] * (g[k][n]*(np.cos(delta[k] - delta[n])) + b[k][n]*np.sin(delta[k] - delta[n]))
+                j2[k-1][n-1] = vmag[k] * (g[k][n]*(np.cos(delta[k] - delta[n])) + b[k][n]*np.sin(delta[k] - delta[n]))
+                j3[k-1][n-1] = -vmag[k] * vmag[n] * (g[k][n]*(np.cos(delta[k] - delta[n])) + b[k][n]*np.sin(delta[k] - delta[n]))
                 j4[k-1][n-1] = vmag[k] * (g[k][n]*(np.sin(delta[k] - delta[n])) - b[k][n]*np.cos(delta[k] - delta[n]))
+
+def simplify_jacobian(n_buses, pv_idx, jacobian): 
+    #simplifies jacobian matrix in the presence of PV-busses by deleting rows and columns
+    if pv_idx.size != 0:
+        jacobian_calc = np.delete(jacobian, pv_idx + n_buses - 2, 0) #n - 2 because bus 1 is index 0 in the jacobian matrix
+        jacobian_calc = np.delete(jacobian_calc, pv_idx + n_buses - 2, 1) #and the submatrices are (n-1) * (n-1)
+    else:
+        jacobian_calc = jacobian
+    return jacobian_calc
 
 def calculate_power_vecs(n_buses, vmag, delta, b, g, pv_idx):
     ###Note! (should probably account for whether bus is load or generation (+/- on pset/qset))
@@ -187,20 +152,14 @@ def check_convergence(y, threshold):
     return np.all(np.absolute(y) < threshold) 
 
 
-def check_pv_bus(buses, q, pv_recalc_flag=False):
+def check_pv_bus(pv_idx, pq_idx, qlim):
     #check if PV bus reactive power is within specified limits
-    #if not, set bus(es) to PQ at Q limit and compute bus voltage
+    #if not, set bus(es) to PQ at Q limit and return a bool to specify whether recalculation should be performed
     pass
 
 
-def simplify_jacobian(n_buses, pv_idx, jacobian): 
-    #simplifies jacobian matrix in the presence of PV-busses by deleting rows and columns
-    if pv_idx.size != 0:
-        jacobian_calc = np.delete(jacobian, pv_idx + n_buses - 2, 0) #n - 2 because bus 1 is index 0 in the jacobian matrix
-        jacobian_calc = np.delete(jacobian_calc, pv_idx + n_buses - 2, 1) #and the submatrices are (n-1) * (n-1)
-    else:
-        jacobian_calc = jacobian
-    return jacobian_calc
+##############################################################################################
+#CLASSES:
 
 class Bus:
     ##INCOMPLETE